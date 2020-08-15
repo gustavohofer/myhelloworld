19a6e3758db8d602eae8e75340779773e33fe5ae
Gustavo <61704099+gustavohofer@users.noreply.github.com>
2020-08-15 13:30:23 -0300

    Update usuario.php

--- 04PDO/views/modules/usuario.php
+++ 04PDO/views/modules/usuario.php
@@ -38,7 +38,7 @@
 				<tr>
 					<th>#</th>
 					<th>Nombre</th>
-					<!--<th>ID</th>-->
+					<!--<th>token</th>-->
 					<th>Email</th>
 					<th>Fecha Creacion</th>
 					<th>Acciones</th>
@@ -50,7 +50,7 @@
 					<tr>
 						<td><?php echo ($key+1)?></td>
 						<td><?php echo $value["usuario"]?></td>
-						<!--<td><?php #echo $value["id"]?></td>-->
+						<!--<td><?php #echo $value["token"]?></td>-->
 						<td><?php echo $value["email"]?></td>
 						<td><?php echo $value["fecha_registro"]?></td>
 						<td>
@@ -59,11 +59,11 @@
 								<!--<button type="button" class="btn btn-warning">Editar <i class="far fa-edit"></i></button> Este boton lo cambio por un anchor y le agrego un href parad ireccionarlo a una pgagina nueva de edicion por otra parte debo pasarle la variable GET de ID a fin de poder reconocer la fila a editar -->
 								<!--<button type="button" class="btnn btn-warning">Editar <i class="far fa-edit"></i></butto>Cambio por una etiqueta anchor, no hay problema mientras el la clase sea btn, tbn le agrego un href para que me pase la variable GET direccion a la pagina de edicion. para poder hacer la edicion necesito que me pase otra variable GET con el ID del usuario, las otras variables GET se pasan agregand "&" en este caso el id lo paso con un etiqueta php-->
 								<div class="px-1" >
-									<a href="index.php?action=editar&id=<?php echo $value["id"];?>" class="btn btn-warning">Editar <i class="far fa-edit"></i></a><!-- En este caso estoy enviando el ID como una variable GET es decir atraves de la URL, una forma mas segura es hacerlo con un formulario POST-->
+									<a href="index.php?action=editar&token=<?php echo $value["token"];?>" class="btn btn-warning">Editar <i class="far fa-edit"></i></a><!-- En este caso estoy enviando el ID como una variable GET es decir atraves de la URL, una forma mas segura es hacerlo con un formulario POST-->
 								</div>
 									<form method = "post"> <!--En este caso envio la info con un formulario POST-->
 										
-										<input type="hidden" value = "<?php echo $value["id"];?>" name="registroEliminar">
+										<input type="hidden" value = "<?php echo $value["token"];?>" name="registroEliminar">
 										
 										<button onclick="return confirm('Esta seguro que desea borrar este registro?');" type="submit" class="btn btn-danger">Borrar <i class="far fa-trash-alt"></i></button>
 										
